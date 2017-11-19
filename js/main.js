28a6f0f73cb77854a5655c876759424f4b63b774
Hannah Helms <helmshannahpatrice@gmail.com>
2017-11-19 14:17:07 -0500

    Change styling on paragraphs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
     $('img').remove();
+
+    var paragraphs = $('p');
+
+    paragraphs.css({
+        'color': 'blue',
+        'font-size': '10px',
+        'text-decoration': 'line-through',
+    });
 });
